d9e471b79bcade21af353af6ee0f5a9aa204a63b
james14kr <hwangms14kr@naver.com>
2026-04-20 15:12:53 +0900

    chore: API base URL 환경변수로 변경

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import * as SecureStore from 'expo-secure-store';
 
-const BASE_URL = "http://192.168.30.109:8080/api";
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export const api = axios.create({
   baseURL: BASE_URL,
