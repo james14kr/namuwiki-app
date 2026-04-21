dbec3d882b9fc2d753bd2b9fe9ee4fdb091a2c32
james14kr <hwangms14kr@naver.com>
2026-04-21 09:19:56 +0900

    feat: 탭 아이콘 설정

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -10,7 +10,7 @@ const Login = () => {
 
   //임시 로그인 - 나중에 실제 API로 교체 예정
   const handleFarmerLogin = () => {
-    router.replace('/(farmer-tabs)/home' as any)
+    router.replace('/(farmer-tabs)/index' as any)
   }
 
   const handleUserLogin = () => {
--- app/(farmer-tabs)/_layout.tsx
+++ app/(farmer-tabs)/_layout.tsx
@@ -1,8 +1,8 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-
 import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
@@ -20,35 +20,35 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color="black" />,
         }}
       />
       <Tabs.Screen
         name="farm"
         options={{
           title: '내 농장',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="leaf.fill" color={color} />,
+          tabBarIcon: ({ color }) => <AntDesign name="unordered-list" size={24} color="black" />,
         }}
       />
       <Tabs.Screen
         name="plant"
         options={{
           title: '식물식별',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="camera.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons name="leaf-sharp" size={24} color="black" />,
         }}
       />
       <Tabs.Screen
         name="dm"
         options={{
           title: '채팅',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="message.fill" color={color} />,
+          tabBarIcon: ({ color }) => <AntDesign name="wechat" size={24} color="black" />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: '프로필',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons name="person-circle-outline" size={24} color="black" />,
         }}
       />
       
